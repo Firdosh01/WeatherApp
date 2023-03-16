2fdd8707e0748246322bdee00e8134d658febf25
Firdosh01 <firdosh7392@gmail.com>
2023-03-16 03:07:42 -0500

    little logic

---
+++ app.js
@@ -0,0 +1,53 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-userWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]")
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+
+let oldTab = userTab;
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+oldTab.classList.add("current-tab");
+getfromSessionStorage();
+
+function switchTab(newTab) {
+    if(newTab != oldTab) {
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab
+        oldTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains ("active")) {
+            userInfoContainer.classList.remove("active")
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active")
+        }else{
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active")
+
+            getfromSessionStorage();
+        }
+    }
+}
+
+userTab.addEventListener("click", () => {
+    switchTab(userTab)
+});
+
+searchTab.addEventListener("click", () => {
+    switchTab(searchTab);
+})
+
+function getfromSessionStorage() {
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates) {
+        grantAccessContainer.classList.add("active");
+    }
+    else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+
+}
